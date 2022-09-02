a615f935cfa4c9b16d4bb4cb4d64ddc4fa63edf6
Milan Mizera <102242396+MilanMizera@users.noreply.github.com>
2022-09-02 16:58:04 +0200

    ss

--- index.php
+++ index.php
@@ -2,17 +2,20 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Kalkulačka</title>
-    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?> ">
+    <link rel="stylesheet" href="style.css?v=<?php
+    echo time(); ?> ">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 <body>
-
-
-
+<?php require_once ("math.php");
+use math\Math;
+?>
 <div class="calculator_container">
     <h1 class="form_title"> Matematická kalkulačka</h1>
 
@@ -20,14 +23,16 @@
         <div class="form-group row">
             <label for="inputEmail3" class="col-sm-2 col-form-label"></label>
             <div class="col">
-                <input value="" type="text" name="number1" class="form-control" id="inputEmail3" placeholder="Zde zadejte Vaše první číslo">
+                <input value="" type="text" name="number1" class="form-control" id="inputEmail3"
+                       placeholder="Zde zadejte Vaše první číslo">
             </div>
         </div>
 
         <div class="form-group row">
             <label for="inputPassword3" class="col-sm-2 col-form-label"></label>
             <div class="col-sm-10">
-                <input value="" type="text" name="number2" class="form-control" id="inputPassword3" placeholder="Zde zadejte Vaše druhé číslo">
+                <input value="" type="text" name="number2" class="form-control" id="inputPassword3"
+                       placeholder="Zde zadejte Vaše druhé číslo">
             </div>
         </div>
 
@@ -36,28 +41,28 @@
                 <legend class="col-form-label col-sm-2 pt-0"></legend>
                 <div class="col-sm-10">
                     <div class="form-check">
-                        <input class="form-check-input" type="radio" name="g" id="gridRadios1" value="+" checked>
+                        <input class="form-check-input" type="radio" name="operation" id="gridRadios1" value="+" checked>
                         <label class="form-check-label" for="gridRadios1">
                             Sčítání +
                         </label>
                     </div>
 
                     <div class="form-check">
-                        <input class="form-check-input" type="radio" name="g" id="gridRadios2" value="-">
+                        <input class="form-check-input" type="radio" name="operation" id="gridRadios2" value="-">
                         <label class="form-check-label" for="gridRadios2">
                             Odčítání -
                         </label>
                     </div>
 
                     <div class="form-check">
-                        <input class="form-check-input" type="radio" name="g" id="gridRadios3" value="*">
+                        <input class="form-check-input" type="radio" name="operation" id="gridRadios3" value="*">
                         <label class="form-check-label" for="gridRadios2">
                             Násobení x
                         </label>
                     </div>
 
                     <div class="form-check">
-                        <input class="form-check-input" type="radio" name="g" id="gridRadios4" value="/">
+                        <input class="form-check-input" type="radio" name="operation" id="gridRadios4" value="/">
                         <label class="form-check-label" for="gridRadios2">
                             Dělení ÷
                         </label>
@@ -75,7 +80,8 @@
     </form>
 
 
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+            crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ math.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace math;
+
+class Math
+{
+    public function catchNumbers()
+    {
+        if (isset($_POST["submit"])) {
+            $operation = $_POST ["operation"];
+            $num1 = $_POST["number1"];
+            $num2 = $_POST ["number2"];
+
+            $num1= str_replace(",",".",$num1);
+            $num2= str_replace(",",".",$num2);
+        }
+
+        if (is_numeric($num1) && is_numeric($num2))
+        {
+          if ($operation == "+") {
+
+              addition($num1, $num2);
+          }
+
+          elseif
+              ($operation == "-"){
+              subtraction($num1, $num2);
+          }
+
+          elseif
+              ($operation == "/"){
+              divided($num1, $num2);
+          }
+
+          elseif
+              ($operation == "*"){
+              multiplication($num1, $num2);
+          }
+        }
+
+        else {
+            $result= "Zadejte pouze čísla";
+        }
+    }
+
+    public function addition($num1, $num2)
+    {
+        $result = $num1 + $num2;
+    }
+
+    public function subtraction($num1, $num2)
+    {
+        $result = $num1 - $num2;
+    }
+
+    public function divided($num1, $num2)
+    {
+        $result = $num1 / $num2;
+    }
+
+    public function multiplication($num1, $num2)
+    {
+        $result = $num1 * $num2;
+    }
+
+}
