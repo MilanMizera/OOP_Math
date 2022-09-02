76c72ee000fa9e25b2288f0b2204f13ed5997479
Milan Mizera <102242396+MilanMizera@users.noreply.github.com>
2022-09-02 17:03:26 +0200

    Update math.php

--- math.php
+++ math.php
@@ -11,35 +11,32 @@ class Math
             $num1 = $_POST["number1"];
             $num2 = $_POST ["number2"];
 
-            $num1= str_replace(",",".",$num1);
-            $num2= str_replace(",",".",$num2);
+            $num1 = str_replace(",", ".", $num1);
+            $num2 = str_replace(",", ".", $num2);
         }
 
-        if (is_numeric($num1) && is_numeric($num2))
-        {
-          if ($operation == "+") {
+        if (is_numeric($num1) && is_numeric($num2)) {
 
-              addition($num1, $num2);
-          }
+            if ($operation == "+") {
+                addition($num1, $num2);
+            }
 
-          elseif
-              ($operation == "-"){
-              subtraction($num1, $num2);
-          }
+            elseif ($operation == "-") {
+                subtraction($num1, $num2);
+            }
 
-          elseif
-              ($operation == "/"){
-              divided($num1, $num2);
-          }
+            elseif
+            ($operation == "/") {
+                divided($num1, $num2);
+            }
 
-          elseif
-              ($operation == "*"){
-              multiplication($num1, $num2);
-          }
-        }
+            elseif
+            ($operation == "*") {
+                multiplication($num1, $num2);
+            }
 
-        else {
-            $result= "Zadejte pouze čísla";
+        } else {
+            $result = "Zadejte pouze čísla";
         }
     }
 
